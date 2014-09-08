93c43f3d24ba10307bd8892fc68a3e23f787e360
zouyixiong <zouyixiong@gmail.com>
2014-09-08 23:40:07 +0800

    完成curl命令调用的demo

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var fetcher = require('../lib/main');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
+    var urls = ["http://tech.qq.com/a/20140808/061411.htm",
+                "http://tech.qq.com/a/20140808/011521.htm",
+                "http://tech.qq.com/a/20140808/022101.htm"];
+    fetcher.fetchURL(urls);
+
   res.send('respond with a resource');
 });
 
